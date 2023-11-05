7dc9a021b65e943e88f48904866a6bc0dd60cefb
NEPALII <60202024+NEPALII@users.noreply.github.com>
2023-11-05 12:09:43 -0500

    Added backend for getting reviews by user or track
    get_reviews_by_user.php
    - Added PHP script to get all reviews made by the currently logged-in user. Returns review data as JSON to use with JavaScript for dynamic pages. See test_get_reviews.html for an example.
    
    get_reviews_by_track.php
    - Added PHP script to get all reviews for a Spotify track. The Spotify track ID is passed using HTTP POST and is equal to the ID used by the Spotify API. Returns review data as JSON to use with JavaScript for dynamic pages. See test_get_reviews.html for an example.
    
    test_get_reviews.html
    - Added test script for testing getting reviews by user or track. Prints JSON data structure for results.

---
+++ Music-Box/static/get_reviews_by_track.php
@@ -0,0 +1,44 @@
+<?php
+    include "dbconfig.php";
+
+    // Default to false to indicate failure
+    $php_return = "false";
+
+    session_start();
+
+    if (!isset($_POST["track_spotify_id"]))
+    {
+        exit("false"); // Failed
+    }
+
+    $mysql = mysqli_connect($server, $login, $password, $dbname);
+
+    if (!$mysql)
+    {
+        exit("false"); // Failed
+    }
+
+    // Get all notications for this user as associative array for JSON decoding
+    $stmt = mysqli_prepare($mysql, "SELECT review_id, user_id, track_spotify_id, review_text, review_time FROM REVIEWS WHERE track_spotify_id=?;");
+    mysqli_stmt_bind_param($stmt, "s", $_POST["track_spotify_id"]);
+    mysqli_stmt_bind_result($stmt, $result_review_id, $result_user_id, $result_track_spotify_id, $result_review_text, $result_review_time);
+
+    if (mysqli_stmt_execute($stmt))
+    {
+        // Will be empty if no results are found
+        $php_return = array("reviews" => []);
+
+        while (mysqli_stmt_fetch($stmt))
+        {
+            array_push($php_return["reviews"], array(
+                "review_id" => $result_review_id,
+                "user_id" => $result_user_id,
+                "track_spotify_id" => $result_track_spotify_id,
+                "review_text" => $result_review_text,
+                "review_time" => $result_review_time
+            ));
+        }
+    }
+
+    echo json_encode($php_return);
+?>
---
+++ Music-Box/static/get_reviews_by_user.php
@@ -0,0 +1,45 @@
+<?php
+    include "dbconfig.php";
+
+    // Default to false to indicate failure
+    $php_return = "false";
+
+    session_start();
+
+    // User ID of currently logged-in user is "id" session variable
+    if (!isset($_SESSION["id"]))
+    {
+        exit("false"); // Failed
+    }
+
+    $mysql = mysqli_connect($server, $login, $password, $dbname);
+
+    if (!$mysql)
+    {
+        exit("false"); // Failed
+    }
+
+    // Get all notications for this user as associative array for JSON decoding
+    $stmt = mysqli_prepare($mysql, "SELECT review_id, user_id, track_spotify_id, review_text, review_time FROM REVIEWS WHERE user_id=?;");
+    mysqli_stmt_bind_param($stmt, "i", $_SESSION["id"]);
+    mysqli_stmt_bind_result($stmt, $result_review_id, $result_user_id, $result_track_spotify_id, $result_review_text, $result_review_time);
+
+    if (mysqli_stmt_execute($stmt))
+    {
+        // Will be empty if no results are found
+        $php_return = array("reviews" => []);
+
+        while (mysqli_stmt_fetch($stmt))
+        {
+            array_push($php_return["reviews"], array(
+                "review_id" => $result_review_id,
+                "user_id" => $result_user_id,
+                "track_spotify_id" => $result_track_spotify_id,
+                "review_text" => $result_review_text,
+                "review_time" => $result_review_time
+            ));
+        }
+    }
+
+    echo json_encode($php_return);
+?>
