ae54f8f21eaf6853a42b10f9b391910faaf69201
NEPALII <60202024+NEPALII@users.noreply.github.com>
2023-12-11 22:37:38 -0500

    Fixes explicit checkbox for music search
    musicSearch.php
    - Fixes explicit checkbox not working.
    
    get_reviews_by_user.php
    - Returns track information fetched via Spotify API instead of just Spotify API track ID.

--- Music-Box/static/get_reviews_by_user.php
+++ Music-Box/static/get_reviews_by_user.php
@@ -1,5 +1,6 @@
 <?php
     include "dbconfig.php";
+    include "spotify_api_config.php";
 
     // Default to false to indicate failure
     $php_return = "false";
@@ -20,9 +21,9 @@
     }
 
     // Get all notications for this user as associative array for JSON decoding
-    $stmt = mysqli_prepare($mysql, "SELECT review_id, user_id, track_spotify_id, review_text, review_time, review_rating FROM REVIEWS WHERE user_id=?;");
+    $stmt = mysqli_prepare($mysql, "SELECT review_id, user_id, track_spotify_id, review_text, review_time FROM REVIEWS WHERE user_id=?;");
     mysqli_stmt_bind_param($stmt, "i", $_SESSION["id"]);
-    mysqli_stmt_bind_result($stmt, $result_review_id, $result_user_id, $result_track_spotify_id, $result_review_text, $result_review_time, $result_review_rating);
+    mysqli_stmt_bind_result($stmt, $result_review_id, $result_user_id, $result_track_spotify_id, $result_review_text, $result_review_time);
 
     if (mysqli_stmt_execute($stmt))
     {
@@ -31,14 +32,19 @@
 
         while (mysqli_stmt_fetch($stmt))
         {
-            array_push($php_return["reviews"], array(
-                "review_id" => $result_review_id,
-                "user_id" => $result_user_id,
-                "track_spotify_id" => $result_track_spotify_id,
-                "review_text" => $result_review_text,
-                "review_time" => $result_review_time,
-                "review_rating" => $result_review_rating
-            ));
+            // Get track information from Spotify API and append to output
+            $track = spotify_api_get_request("v1/tracks/" . $result_track_spotify_id);
+
+            if ($track)
+            {
+                array_push($php_return["reviews"], array(
+                    "review_id" => $result_review_id,
+                    "user_id" => $result_user_id,
+                    "track" => $track,
+                    "review_text" => $result_review_text,
+                    "review_time" => $result_review_time
+                ));
+            }
         }
     }
 
